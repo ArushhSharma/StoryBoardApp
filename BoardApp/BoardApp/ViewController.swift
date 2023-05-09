d95a5fadf9e021ce402ce170b389e56a3e53fce3
ArushhSharma <132982594+ArushhSharma@users.noreply.github.com>
2023-05-09 22:06:58 +0530

    Add files via upload

---
+++ BoardApp/BoardApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BoardApp
+//
+//  Created by Arush Sharma on 09/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SecondPage(_ sender: Any) {
+    }
+    
+}
+
